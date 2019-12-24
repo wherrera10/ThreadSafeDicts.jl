2853e34922899e70b3dc54493242cad7d497dbdd
Bill <wherrera10@users.noreply.github.com>
2019-12-24 00:46:04 -1000

    Update ThreadSafeDicts.jl

--- src/ThreadSafeDicts.jl
+++ src/ThreadSafeDicts.jl
@@ -3,7 +3,7 @@ module ThreadSafeDicts
 using Distributed
 
 import Base.getindex, Base.setindex!, Base.get!, Base.get, Base.empty!, Base.pop!
-import Base.delete!, Base.print, Base.iterate
+import Base.haskey, Base.delete!, Base.print, Base.iterate, Base.length
 
 export ThreadSafeDict
 
@@ -15,8 +15,8 @@ arguments to the d member Dict, unlock the spinlock, and then return what is ret
 struct ThreadSafeDict{K, V} <: AbstractDict{K, V}
     dlock::Threads.SpinLock
     d::Dict
-    ThreadSafeDict{K, V}() where V where K = ThreadSafeDict(Threads.SpinLock(), Dict{K, V}())
-    ThreadSafeDict{K, V}(itr) where V where K = ThreadSafeDict(Threads.SpinLock(), Dict{K, V}(itr))
+    ThreadSafeDict{K, V}() where V where K = new(Threads.SpinLock(), Dict{K, V}())
+    ThreadSafeDict{K, V}(itr) where V where K = new(Threads.SpinLock(), Dict{K, V}(itr))
 end
 
 function getindex(dic::ThreadSafeDict, k)
@@ -26,9 +26,9 @@ function getindex(dic::ThreadSafeDict, k)
     return v
 end
 
-function setindex!(dic::ThreadSafeDict, v, k)
+function setindex!(dic::ThreadSafeDict, k, v)
     lock(dic.dlock)
-    h = setindex!(dic.d, v,  k)
+    h = setindex!(dic.d, k, v)
     unlock(dic.dlock)
     return h
 end
@@ -42,7 +42,7 @@ end
 
 function get(dic::ThreadSafeDict, k)
     lock(dic.dlock)
-    v = get(dic.d, k, v)
+    v = get(dic.d, k)
     unlock(dic.dlock)
     return v
 end
@@ -56,7 +56,7 @@ end
 
 function pop!(dic::ThreadSafeDict)
     lock(dic.dlock)
-    p = pop!(dic.d, k, v)
+    p = pop!(dic.d)
     unlock(dic.dlock)
     return p
 end
@@ -75,6 +75,13 @@ function delete!(dic::ThreadSafeDict, k)
     return p
 end
 
+function length(dic::ThreadSafeDict)
+    lock(dic.dlock)
+    len = length(dic.d)
+    unlock(dic.dlock)
+    return len
+end
+
 function iterate(dic::ThreadSafeDict)
     lock(dic.dlock)
     p = iterate(dic.d)
@@ -89,5 +96,11 @@ function iterate(dic::ThreadSafeDict, i)
     return p
 end  
 
+function print(io::IO, dic::ThreadSafeDict)
+    print(io, "Dict was ", islocked(dic.dlock) ? "locked" : "unlocked", ", contents: ")
+    lock(dic.dlock)
+    print(io, dic.d)
+    unlock(dic.dlock)
+end
 
 end # module
