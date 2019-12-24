181f4617c40d6437255e4b4aae7dac9ed9e232a3
Bill <wherrera10@users.noreply.github.com>
2019-12-23 22:29:09 -1000

    Initial code

---
+++ src/ThreadSafeDicts.jl
@@ -0,0 +1,92 @@
+module ThreadSafeDicts
+
+using Distributed
+
+import Main.getindex, Main.setindex!, Main.get!, Main.get, Main.empty!, Main.pop!
+import Main.delete!, Main.print, Main.iterate
+
+""" 
+    ThreadSafeDict(pairs::Vector{Pair{K,V}})   
+
+Struct and constructor for ThreadSafeDict. There is one lock per Dict struct. All functions lock this lock, pass 
+arguments to the d member Dict, unlock the spinlock, and then return what is returned by the Dict.
+"""
+struct ThreadSafeDict{K, V} <: AbstractDict{K, V}
+    dlock::Threads.SpinLock
+    d::Dict
+    ThreadSafeDict{K, V}() where V where K = ThreadSafeDict(SpinLock(), Dict{K, V}())
+    ThreadSafeDict{K, V}(itr) where V where K = ThreadSafeDict(SpinLock(), Dict{K, V}(itr))
+end
+
+function getindex(dic::ThreadSafeDict, k)
+    lock(dic.dlock)
+    v = getindex(dic.d, k)
+    unlock(dic.dlock)
+    return v
+end
+
+function setindex!(dic::ThreadSafeDict, v, k)
+    lock(dic.dlock)
+    h = setindex!(dic.d, v,  k)
+    unlock(dic.dlock)
+    return h
+end
+
+function haskey(dic::ThreadSafeDict, k)
+    lock(dic.dlock)
+    b = haskey(dic.d, k)
+    unlock(dic.dlock)
+    return b
+end
+
+function get(dic::ThreadSafeDict, k)
+    lock(dic.dlock)
+    v = get(dic.d, k, v)
+    unlock(dic.dlock)
+    return v
+end
+
+function get!(dic::ThreadSafeDict, k, v)
+    lock(dic.dlock)
+    v = get!(dic.d, k, v)
+    unlock(dic.dlock)
+    return v
+end
+
+function pop!(dic::ThreadSafeDict)
+    lock(dic.dlock)
+    p = pop!(dic.d, k, v)
+    unlock(dic.dlock)
+    return p
+end
+
+function empty!(dic::ThreadSafeDict)
+    lock(dic.dlock)
+    d = empty!(dic.d)
+    unlock(dic.dlock)
+    return d
+end
+
+function delete!(dic::ThreadSafeDict, k)
+    lock(dic.dlock)
+    p = delete!(dic.d, k)
+    unlock(dic.dlock)
+    return p
+end
+
+function iterate(dic::ThreadSafeDict)
+    lock(dic.dlock)
+    p = iterate(dic.d)
+    unlock(dic.dlock)
+    return p
+end
+
+function iterate(dic::ThreadSafeDict, i)
+    lock(dic.dlock)
+    p = iterate(dic.d, i)
+    unlock(dic.dlock)
+    return p
+end  
+
+
+end # module
