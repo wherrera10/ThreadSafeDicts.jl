ee543b01d037c391ac2a61420680230ac8b6ea34
Bill <wherrera10@users.noreply.github.com>
2026-09-10 20:45:38 -1000

    Enhance thread tests in runtests.jl
    Added conditional thread tests based on CPU threads.

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,10 +1,23 @@
 using PerformanceTestTools
 
+threadtests = Any[
+    nothing,
+    ["JULIA_NUM_THREADS" => Threads.nthreads() > 1 ? "1" : "2"],
+]
+
+ncpus = Sys.CPU_THREADS
+
+if ncpus >= 4
+    push!(threadtests, ["JULIA_NUM_THREADS" => "4"])
+end
+
+if ncpus >= 8
+    push!(threadtests, ["JULIA_NUM_THREADS" => "8"])
+end
+
 PerformanceTestTools.@include_foreach(
     "test_threaded.jl",
-    [nothing,
-    ["JULIA_NUM_THREADS" => Threads.nthreads() > 1 ? "1" : "2"],
-    ],
+    threadtests,
 )
 
 PerformanceTestTools.@include_foreach(
