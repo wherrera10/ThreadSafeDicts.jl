a410b57c6cc00f4173fa90664e915bf9a1e2f8f0
Bill <wherrera10@users.noreply.github.com>
2026-09-10 21:53:17 -1000

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -1,3 +1,24 @@
 using Documenter, ThreadSafeDicts
 
-makedocs(sitename = "ThreadSafeDicts Module Documentation", format = Documenter.HTML(prettyurls = false))
+DocMeta.setdocmeta!(ThreadSafeDicts, :DocTestSetup, :(using ThreadSafeDicts); recursive=true)
+
+makedocs(;
+    modules=[ThreadSafeDicts],
+    authors="William Herrera and various patch submitters",
+    sitename="ThreadSafeDicts.jl Documentation",
+    format=Documenter.HTML(;
+        canonical="https://wherrera10.github.io/ThreadSafeDicts.jl",
+        edit_link="main",
+        assets=String[],
+    ),
+    pages=[
+        "Home" => "index.md",
+        # Add future pages here, e.g., "API Reference" => "api.md"
+    ],
+)
+
+deploydocs(;
+    repo="github.com/wherrera10/ThreadSafeDicts.jl.git",
+    devbranch="main", # Or "master" depending on your default branch
+)
+
