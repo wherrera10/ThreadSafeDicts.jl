ccc71f59b91ec1d6da09cc68b106267f9bdb81b2
Bill <wherrera10@users.noreply.github.com>
2026-09-13 19:45:40 -1000

    Refactor Szymański test for thread safety improvements
    Changes are primarily to make the test run faster.

--- test/Szymański.jl
+++ test/Szymański.jl
@@ -1,62 +1,83 @@
 using Test
+using ThreadSafeDicts
 
-using ThreadSafeDicts  # implement a single lock on all shared values as a task Dict
-
-const iddict = ThreadSafeDict{Int, Int}()
-flag(id) = get(iddict, id, 0)
+const iddict = ThreadSafeDict{Int,Int}()
 
 """ test the implementation on each thread, concurrently """
-function runSzymański(id, allszy, tdict, inside)
-    others = filter(!=(id), allszy)
-    iddict[id] = 1                            # Standing outside waiting room
-    while !all(t -> flag(t) < 3, others)      # Wait for open door
-        yield()
+function runSzymański(id, N, inside, violations, results, threadids)
+    flag(id) = get(iddict, id, 0)
+
+    iddict[id] = 1                         # Standing outside waiting room
+
+    while any(t -> t != id && flag(t) >= 3, 1:N)
+        yield()                            # Wait for open door
     end
-    iddict[id] = 3                            # Standing in doorway
-    if any(t -> flag(t) == 1, others)         # Another process is waiting to enter
-        iddict[id] = 2                        # Waiting for other processes to enter
-        while !any(t -> flag(t) == 4, others) # Wait for a process to enter and close the door
-            yield()
+
+    iddict[id] = 3                         # Standing in doorway
+
+    if any(t -> t != id && flag(t) == 1, 1:N)
+        iddict[id] = 2                     # Waiting for other processes to enter
+
+        while !any(t -> t != id && flag(t) == 4, 1:N)
+            yield()                        # Wait for a process to enter and close the door
         end
     end
-    iddict[id] = 4                            # The door is closed
-    for t in others                           # Wait for everyone of lower ID to finish exit 
+
+    iddict[id] = 4                         # The door is closed
+
+    for t in 1:N                           # Wait for everyone of lower ID to finish exit
         t >= id && continue
+
         while flag(t) > 1
             yield()
         end
     end
 
     # critical section
-    Threads.atomic_add!(inside, 1)
-    @test inside[] == 1
-    tid = Threads.threadid()
-    tdict[tid] = vcat(get!(tdict, tid, Int[]), id)
-    Threads.atomic_add!(inside, -1)
+    old = Threads.atomic_add!(inside, 1)
+    old != 0 && Threads.atomic_add!(violations, 1)
+
+    results[id] = true
+    threadids[id] = Threads.threadid()
+
     id % 100 == 0 && print(id, "...\b\b\b\b\b\b\b\b\b")
+
+    Threads.atomic_add!(inside, -1)
+
     # end critical section
 
     # Exit protocol
-    for t in others                           # Ensure everyone in the waiting room has
-        t <= id && continue
-        while flag(t) ∉ [0, 1, 4]             # realized that the door is supposed to be closed
+    for t in 1:N                           # Ensure everyone in the waiting room has
+        t <= id && continue                # realized that the door is supposed to be closed
 
+        while true
+            x = flag(t)
+            (x == 0 || x == 1 || x == 4) && break
             yield()
         end
     end
-    iddict[id] = 0                            # Leave. Reopen door if nobody is still in the waiting room
+
+    iddict[id] = 0                         # Leave. Reopen door if nobody is still in the waiting room
 end
 
 function test_Szymański(N)
     empty!(iddict)
-    tdict = Dict{Int, Vector{Int}}()
+
     inside = Threads.Atomic{Int}(0)
-    allszy = collect(1:N)
-    @Threads.threads for i in eachindex(allszy)
-        runSzymański(i, allszy, tdict, inside)
+    violations = Threads.Atomic{Int}(0)
+    results = falses(N)
+    threadids = Vector{Int}(undef, N)
+
+    Threads.@threads for id in 1:N
+        runSzymański(id, N, inside, violations, results, threadids)
     end
+
+    println()
+
     @test inside[] == 0
-    @test 1:N == reduce(vcat, values(tdict)) |> sort!
+    @test violations[] == 0
+    @test all(results)
+    @test all(>(0), threadids)
 end
 
 test_Szymański(2000)
