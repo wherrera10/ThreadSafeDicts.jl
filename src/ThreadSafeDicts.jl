89b16e4b448c4639720405e415e7179112ce3cd5
Bill <wherrera10@users.noreply.github.com>
2026-09-18 08:06:41 -1000

    Clarify lock type in ThreadSafeDict documentation
    Updated documentation to specify ReentrantLock instead of just lock.

--- src/ThreadSafeDicts.jl
+++ src/ThreadSafeDicts.jl
@@ -9,8 +9,8 @@ export ThreadSafeDict
 """ 
     ThreadSafeDict(pairs::Vector{Pair{K,V}})
 
-Struct and constructor for ThreadSafeDict. There is one lock per Dict struct. All functions lock this lock, pass 
-arguments to the d member Dict, unlock the spinlock, and then return what is returned by the Dict.
+Struct and constructor for ThreadSafeDict. There is one ReentrantLock per Dict struct. All functions lock this lock, pass 
+arguments to the d member Dict, unlock the lock, and then return what is returned by the Dict.
 """
 struct ThreadSafeDict{K, V} <: AbstractDict{K, V}
     dlock::ReentrantLock
